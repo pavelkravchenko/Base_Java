b3a771d62373c7eaae7e40df860c8ea46062507d
Kravchenko Pavel <paxa.kravchenko@gmail.com>
2015-07-22 22:26:29 +0300

    Update Task35.java

--- src/Task35.java
+++ src/Task35.java
@@ -7,7 +7,7 @@ public class Task35 {
     }
 
     public static boolean isPrimeNumber(int n) {
-        if ((n <= 1) || (n % 2 == 0 && n != 2) || (n % 3 == 0 && n != 3) || (n % 5 == 0 && n != 5)) {
+        if ((n <= 1) || (n % 2 == 0 && n != 2) || (n % 3 == 0 && n != 3) || (n % 5 == 0 && n != 5) || (n % 7 == 0 && n != 7)) {
             return false;
         }
         return true;

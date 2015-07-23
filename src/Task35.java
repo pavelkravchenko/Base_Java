7e0ea46d455433b9dc12f53a2c2bb773c16ee3b0
Kravchenko Pavel <paxa.kravchenko@gmail.com>
2015-07-23 15:18:25 +0300

    Update Task35.java

--- src/Task35.java
+++ src/Task35.java
@@ -1,15 +1,23 @@
+/**
  * 35.	Дано целое число N (> 1). Если оно является простым, то есть не имеет положительных делителей,
  * кроме 1 и самого себя, то вывести True, иначе вывести False.
  */
 public class Task35 {
     public static void main(String[] args) {
-        System.out.println(isPrimeNumber(4));
+        System.out.println(isPrimeNumber(49));
     }
 
     public static boolean isPrimeNumber(int n) {
-        if ((n <= 1) || (n % 2 == 0 && n != 2) || (n % 3 == 0 && n != 3) || (n % 5 == 0 && n != 5) || (n % 7 == 0 && n != 7)) {
-            return false;
-        }
-        return true;
+       if (n<=1) {
+           return false;
+       }
+        for(int i=2; i<n; i++){
+           if ( n%i==0){
+               return false;
+           }
+       }return true;
     }
 }
+
+
+

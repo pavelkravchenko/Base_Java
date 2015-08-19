edc2ef1b0f20d01690b872ba6f5d63f9edc209e8
Kravchenko Pavel <paxa.kravchenko@gmail.com>
2015-08-19 18:56:41 +0300

    Update Task2ExtJava.java

--- Task2ExtJava.java
+++ Task2ExtJava.java
@@ -16,29 +16,24 @@ public class Task2ExtJava {
     }
 
     public static boolean checkBrackets(String expression) {
-        char[] chars = expression.toCharArray();
+        char[] inputChars = expression.toCharArray();
         char[] stack = new char[expression.length()];
-        int curHead = -1;
+        int stackHead = -1;
         for (int i = 0; i < expression.length(); i++) {
-            if (isOpenBracket(chars[i])) {
-                curHead++;
-                stack[curHead] = chars[i];
-            } else if (isClosedBracket(chars[i])) {
-                if (curHead < 0) {
+            if (isOpenBracket(inputChars[i])) {
+                stackHead++;
+                stack[stackHead] = inputChars[i];
+            } else if (isClosedBracket(inputChars[i])) {
+                if (stackHead < 0) {
                     return false;
-                } else if (stack[curHead] == getOpenedBracket(chars[i])) {
-                    //stack[curHead] = ' ';
-                    curHead--;
+                } else if (stack[stackHead] == getOpenedBracketByClosed(inputChars[i])) {
+                    stackHead--;
                 } else return false;
             }
         }
 
-        if (curHead == -1) {
-            return true;
-        }
-        return false;
+        return (stackHead == -1);
     }
-
 
     public static boolean isOpenBracket(char bracket) {
         if (bracket == '(' || bracket == '{' || bracket == '[') {
@@ -54,14 +49,7 @@ public class Task2ExtJava {
         return false;
     }
 
-    /*public static char isPairBrackets(char bracket1, char bracket2) {
-        if ((bracket1 == '(' && bracket2 == ')') ||
-                (bracket1 == '[' && bracket2 == ']') ||
-                (bracket1 == '{' && bracket2 == '}')) {
-            return bracket2;
-        } else return 0;
-    }*/
-    public static char getOpenedBracket(char bracket) {
+    public static char getOpenedBracketByClosed(char bracket) {
         if (bracket == '}') {
             return '{';
         }
@@ -70,6 +58,7 @@ public class Task2ExtJava {
         }
         if (bracket == ')') {
             return '(';
-        }  throw new RuntimeException("input value is not closed bracket");
+        }
+        throw new RuntimeException("input value is not closed bracket");
     }
 }
